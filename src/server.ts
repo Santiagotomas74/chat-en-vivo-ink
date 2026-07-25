2b04201ffd23522287c14b0fa1c41a886da13d25
Santiago Taher <chrismar747@gmail.com>
2026-07-25 13:01:50 -0300

    chat en vivo v3

--- src/server.ts
+++ src/server.ts
@@ -38,3 +38,11 @@ const PORT = process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
   console.log(`🚀 Chat Server escuchando en ${PORT}`);
 });
+
+app.get("/health", (_, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
